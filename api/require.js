a0b84e191e5ba1b7580393ef652a8b56ce71dc53
shprotoculture <shprotoculture@yandex.ru>
2019-02-20 14:06:16 +0300

    3

--- api/require.js
+++ api/require.js
@@ -1,20 +1,8 @@
 import axios from 'axios';
+import { wooConfig } from './woocommerce-api.js';
 
-var WooCommerceAPI = require('woocommerce-api');
-
-var WooCommerce = new WooCommerceAPI({
-    url: 'https://api.dinoshop.msk.ru',
-    consumerKey: 'ck_2f11033dd5c4d4a43f11f5c766dda92994cbd2f3',
-    consumerSecret: 'cs_8aee5928fc84585c665e295ff7b910e7a67f66e8',
-    wpAPI: true,
-    version: 'wc/v3',
-    queryStringAuth: true // Force Basic Authentication as query string true and using under HTTPS
-});
 export default function (req, res, next) {
-    // req is the Node.js http request object
-
-    console.log(req.body);
-
+    
     if(req.body.event === 'payment.waiting_for_capture') {
         let data = {
             id: req.body.object.id,
@@ -36,14 +24,14 @@ export default function (req, res, next) {
             status: 'completed'
         };
 
-        WooCommerce.put('orders/' + req.body.object.metadata.orderID, data, function(err, data, res) {
-            console.log(res);
+        let orderID = req.body.object.metadata.orderID;
+
+        axios.put('orders/' + orderID, data, wooConfig).then(response => {
+            console.log(response);
+        }).catch(error => {
+            console.log(error);
         });
     }
 
-    // res is the Node.js http response object
-
-    // next is a function to call to invoke the next middleware
-    // Don't forget to call next at the end if your middleware is not an endpoint!
     next();
 };
